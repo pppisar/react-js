c86458c6a3ea1c8d27dd5a50a8dd5b4019060344
Oleksii Pysarenko <pysarole@gmail.com>
2024-04-05 11:02:05 +0200

    Fetching data and useCallback

--- src/components/EffectSection.jsx
+++ src/components/EffectSection.jsx
@@ -1,19 +1,29 @@
-import { useState } from 'react'
+import { useEffect, useStat, useCallback } from 'react'
 import Button from './Button/Button'
 import Modal from './Modal/Modal'
 
 export default function EffectSection() {
   const [modal, setModal] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [users, setUsers] = useState([])
 
-  function openModal() { 
-    setModal((prev) => !prev)
-  }
+  const fetchUsers = useCallback(async () => {
+    setLoading(true)
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users = await response.json()
+    setUsers(users)
+    setLoading(false)
+  }, [])
+
+  useEffect(() => {
+    fetchUsers()
+  }, [fetchUsers])
 
   return (
     <section>
       <h3>Effects</h3>
 
-      <Button onClick={openModal}>Open info</Button>
+      <Button style={{marginBottom: '1rem'}} onClick={() => setModal((prev) => !prev)}>Open info</Button>
       
       <Modal open={modal}>
         <h3>Hello from modal</h3>
@@ -21,6 +31,12 @@ export default function EffectSection() {
           Cupiditate voluptate rerum maiores veniam, doloremque dolores asperiores soluta adipisci autem animi.</p>
         <Button onClick={() => setModal()}>Close info</Button>
       </Modal>
+
+      {loading && <p>Loading...</p>}
+      
+      {!loading && <ul>
+        {users.map((user) => <li id={user.id}>{user.name}</li>)}
+      </ul>}
     </section>
   )
 }
